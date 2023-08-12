30b68053f8013f13f80b92fc4613e01ca2442f20
Mayank-Bansal24 <b20156@students.iitmandi.ac.in>
2023-08-13 00:27:55 +0530

    Revert "Join updated"
    This reverts commit 3cb8236bc1cefdde6a5979286e198713733d8fe9.

--- server/index.js
+++ server/index.js
@@ -8,24 +8,12 @@ const PORT=process.env.PORT || 5000;
 const router = require('./router');
 const app= express();
 const server =http.createServer(app);
-const io= socketio(server)(httpServer, {
-    cors: {
-      origin: '*',
-    }
-  });
+const io= socketio(server);
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!! I');
-
-
-
-    socket.on('join', ({ name, room }) => {
-        console.log(name,room);
-      });
-
-
     socket.on('disconnect', () => {
-    console.log('User has left!!!');
+    console.log('User had left!!!');
     })
     });
 app.use(router);
