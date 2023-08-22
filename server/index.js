d82f73e42fb910a50e2e854b2544f18bff2cca9f
Mayank-Bansal24 <b20156@students.iitmandi.ac.in>
2023-08-22 15:45:42 +0530

    Lib install

--- server/index.js
+++ server/index.js
@@ -1,21 +1,7 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 
-const PORT=process.env.PORT || 5000;
-
-const router = require('./router');
-const app= express();
-const server =http.createServer(app);
-const io= socketio(server);
-
-io.on('connection', (socket) => {
-    console.log('We have a new connection!! I');
-    socket.on('disconnect', () => {
-    console.log('User had left!!!');
-    })
-    });
-app.use(router);
-
-server.listen(PORT , () => console.log(`Server has started on port ${PORT}`));
+const app = express();
+require("dotenv").config();
