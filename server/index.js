4ae9ab0643aef49ccf758eaf528374ca833ac9e8
Mayank-Bansal24 <b20156@students.iitmandi.ac.in>
2023-08-22 16:36:34 +0530

    Routes Completed

--- server/index.js
+++ server/index.js
@@ -5,3 +5,23 @@ const mongoose = require("mongoose");
 
 const app = express();
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+// mongoose.connect(process.env.MONGO_URL)
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+})
+.then(()=> {
+    console.log("DB connection done");
+})
+.catch((err) => {
+    console.log(err.message);
+});
+
+const server = app.listen(process.env.PORT, () =>{
+    console.log(`Server started on Port ${process.env.PORT}`)
+})
